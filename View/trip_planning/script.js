50292e71db1ac6e276f3c75ae111de74d0c401e6
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2021-02-12 23:04:27 +0330

    add JAVASCRIPT (DOM) for selecting/manipulating and ADD and Remove cards for pages MEDICAL SERVICES + TOURISM SERVICES + HOTELS + some changes ( it is near the end of developing FRONT of project! )

--- View/trip_planning/script.js
+++ View/trip_planning/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-
-    $("button.addTourButton").on("click",function(){
-
-        $("div:last").after("<div class=item><p>Title</p></div>");
-
-    })
-
-})
+// $(document).ready(function(){
+//
+//     $("button.addTourButton").on("click",function(){
+//
+//         $("div:last").after("<div class=item><p>Title</p></div>");
+//
+//     })
+//
+// })
